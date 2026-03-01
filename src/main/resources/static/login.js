6e4562a8f797db165255c03dff2a4aa65a29e272
Luis Muacho <l.muacho@fct.unl.pt>
2026-03-01 02:32:49 +0000

    Added Login interface and redirection automatically.

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -10,3 +10,34 @@ function fazerLogin() {
 
     window.location.href = 'index.html';
 }
+
+function registerAccount() {
+    const username = document.getElementById('new-username-input').value;
+    if (!username) {
+        alert("Please insert a username.");
+        return;
+    }
+
+    const userRegister = {
+        username: username
+    };
+
+    fetch(`/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userRegister)
+    })
+        .then(response => response.json())
+        .then(apiResponse => {
+            if(apiResponse.success) {
+                alert("✅ Account Created! Your access ID is: " + apiResponse.data);
+                localStorage.setItem('sim_user_id', apiResponse.data);
+                window.location.href = 'index.html';
+            } else {
+                alert("❌ Erro: " + apiResponse.message);
+            }
+        })
+        .catch(error => console.error("Erro no registo:", error));
+}
